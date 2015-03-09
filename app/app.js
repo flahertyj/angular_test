291fbc88ab5bf7a5aec7d9d881d25eacaed9f2c9
flahertyj <flahertyj@gmail.com>
2015-03-09 01:07:59 -0600

    Added factory for grabbing city details and CSS.

--- app/app.js
+++ app/app.js
@@ -6,32 +6,44 @@ angular.module('myApp', [
   'myApp.version'
 ])
 
-.service('CityHelper', ['$http', function($http) {
-  this.nameToId = function(name) {
+.factory('CityHelper', ['$http', '$q', function($http, $q) {
+  var service = {};
+
+  service.nameToId = function(name) {
+    var deferred = $q.defer();
     $http.get('http://mqlocal.aol.com/photos').
       success(function(data) {
         for (var i = 0; i < data.length; i++) {
-          var description = data[i].description
-          if (description) {
+          var description = data[i].description;
+          if (typeof description !== 'undefined') {
             var descArray = description.split(' ');
             var cityName = descArray[descArray.length - 1];// Adding name since description has the id as well so this is simpler.
             if (cityName.toLowerCase() === name.toLowerCase()) {
-              return descArray;
+              deferred.resolve(descArray[0]);
             }
           }
         }
 
-        return -1;
+        deferred.resolve(-1);
       }).
       error(function(data, status, headers, config) {
-        return -1;
+        console.log("ERROR @")
+        deferred.resolve(-1);
       });
+
+      return deferred.promise;
   };
+
+  return service;
 }])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-    when('/city/:cityId', {
+    when('/', {
+      templateUrl: 'partials/cities.html',
+      controller: 'CityController'
+    }).
+    when('/:cityName', {
       templateUrl: 'partials/cityDetails.html',
       controller: 'CityDetailController'
     }).
@@ -41,60 +53,37 @@ angular.module('myApp', [
 }])
 
 .controller('CityController', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://mqlocal.aol.com/photos').
-    success(function(data) {
+  $http.get('http://mqlocal.aol.com/photos')
+    .success(function(data) {
       for (var i = 0; i < data.length; i++) {
         var description = data[i].description
-        if (description) {
+        if (typeof description !== 'undefined') {
           var descArray = description.split(' ')
           data[i].name = descArray[descArray.length - 1]// Adding name since description has the id as well so this is simpler.
         }
       }
 
       $scope.cityList = data;
-    }).
-    error(function(data, status, headers, config) {
-      $scope.cityList = [];
+    })
+    .error(function(data, status, headers, config) {
+      console.log(data);
     });
 }])
 
-.controller('CityDetailController', ['$scope', '$routeParams', '$http', 'CityHelper', function($scope, $routeParams, $http, CityHelper) {
-  var cityId = $routeParams.cityId;
+.controller('CityDetailController', ['$scope', '$routeParams', '$http', 'CityHelper',
+function($scope, $routeParams, $http, CityHelper) {
+  var cityName = $routeParams.cityName;
 
-  if (isNaN(cityId)) {// If cityId route param is not a number, find it by name.
-    $http.get('http://mqlocal.aol.com/photos').
-      success(function(data) {
-        for (var i = 0; i < data.length; i++) {
-          var description = data[i].description
-          if (description) {
-            var descArray = description.split(' ');
-            var cityName = descArray[descArray.length - 1];// Adding name since description has the id as well so this is simpler.
-            if (cityName.toLowerCase() === cityId.toLowerCase()) {
-              $scope.cityId = descArray[0];
-              $http.get('http://mqlocal.aol.com/photos/' + $scope.cityId).
-                success(function(data) {
-                  $scope.cityDetails = data;
-                }).
-                error(function(data, status, headers, config) {
-                  $scope.cityDetails = [];
-                });
-            }
-          }
-        }
-
-        $scope.cityDetails = [];
-      }).
-      error(function(data, status, headers, config) {
-        $scope.cityDetails = [];
-      });
-  }else {
-    $scope.cityId = cityId;
-    $http.get('http://mqlocal.aol.com/photos/' + $scope.cityId).
-      success(function(data) {
-        $scope.cityDetails = data;
-      }).
-      error(function(data, status, headers, config) {
-        $scope.cityDetails = [];
-      });
-  }
+  CityHelper.nameToId(cityName)
+    .then(function(cityId){
+      $http.get('http://mqlocal.aol.com/photos/' + cityId).
+        success(function(data) {
+          $scope.cityDetails = data;
+        }).
+        error(function(data, status, headers, config) {
+          console.log(data);
+        });
+    }, function(data) {
+      console.log(data);
+    })
 }]);
