dec350ce878fe41d42439641f14e763623bdd588
flahertyj <flahertyj@gmail.com>
2015-03-09 08:34:39 -0600

    Added CityDetails service and CityList service to reduce code in controllers.  Also added resolves for routeparams so it wouldn't load view until everything was done loading correctly.

--- app/app.js
+++ app/app.js
@@ -9,6 +9,27 @@ angular.module('myApp', [
 .factory('CityHelper', ['$http', '$q', function($http, $q) {
   var service = {};
 
+  service.cityList = function() {
+    var deferred = $q.defer();
+    $http.get('http://mqlocal.aol.com/photos')
+      .success(function(data) {
+        for (var i = 0; i < data.length; i++) {
+          var description = data[i].description
+          if (typeof description !== 'undefined') {
+            var descArray = description.split(' ')
+            data[i].name = descArray[descArray.length - 1]// Adding name since description has the id as well so this is simpler.
+          }
+        }
+
+        deferred.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        deferred.resolve(data);
+      });
+
+    return deferred.promise;
+  };
+
   service.nameToId = function(name) {
     var deferred = $q.defer();
     $http.get('http://mqlocal.aol.com/photos').
@@ -27,11 +48,29 @@ angular.module('myApp', [
         deferred.resolve(-1);
       }).
       error(function(data, status, headers, config) {
-        console.log("ERROR @")
         deferred.resolve(-1);
       });
 
       return deferred.promise;
+  };
+
+  service.cityDetails = function(cityName) {
+    var deferred = $q.defer();
+
+    this.nameToId(cityName)
+      .then(function(cityId){
+        $http.get('http://mqlocal.aol.com/photos/' + cityId).
+          success(function(data) {
+            deferred.resolve(data);
+          }).
+          error(function(data, status, headers, config) {
+            deferred.resolve(data);
+          });
+      }, function(data) {
+        deferred.resolve(data);
+      })
+
+    return deferred.promise;
   };
 
   return service;
@@ -41,49 +80,32 @@ angular.module('myApp', [
   $routeProvider.
     when('/', {
       templateUrl: 'partials/cities.html',
-      controller: 'CityController'
+      controller: 'CityController',
+      resolve: {
+        cityList: ['CityHelper', function(CityHelper) {
+          return CityHelper.cityList();
+        }]
+      }
     }).
     when('/:cityName', {
       templateUrl: 'partials/cityDetails.html',
-      controller: 'CityDetailController'
+      controller: 'CityDetailController',
+      resolve: {
+        cityDetails: ['CityHelper', '$location', function(CityHelper, $location) {
+          // Couldn't figure out how to just get the city name without '/', I'm sure there's an easier way...
+          return CityHelper.cityDetails($location.path().replace('/', ''));
+        }]
+      }
     }).
     otherwise({
       redirectTo: '/'
     });
 }])
 
-.controller('CityController', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://mqlocal.aol.com/photos')
-    .success(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        var description = data[i].description
-        if (typeof description !== 'undefined') {
-          var descArray = description.split(' ')
-          data[i].name = descArray[descArray.length - 1]// Adding name since description has the id as well so this is simpler.
-        }
-      }
-
-      $scope.cityList = data;
-    })
-    .error(function(data, status, headers, config) {
-      console.log(data);
-    });
+.controller('CityController', ['$scope', 'cityList', function($scope, cityList) {
+  $scope.cityList = cityList;
 }])
 
-.controller('CityDetailController', ['$scope', '$routeParams', '$http', 'CityHelper',
-function($scope, $routeParams, $http, CityHelper) {
-  var cityName = $routeParams.cityName;
-
-  CityHelper.nameToId(cityName)
-    .then(function(cityId){
-      $http.get('http://mqlocal.aol.com/photos/' + cityId).
-        success(function(data) {
-          $scope.cityDetails = data;
-        }).
-        error(function(data, status, headers, config) {
-          console.log(data);
-        });
-    }, function(data) {
-      console.log(data);
-    })
+.controller('CityDetailController', ['$scope', 'cityDetails', function($scope, cityDetails) {
+  $scope.cityDetails = cityDetails;
 }]);
